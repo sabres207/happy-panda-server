283ec5de26d142fbc01be3ee77481dc19cd1bb8c
dorons <sagesdoron@gmail.com>
2016-08-13 03:19:21 +0300

    first stage project

---
+++ apigateway.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from dishescontroller import dishes_controller
+from userscontroller import users_controller
+
+
+app = Flask(__name__)
+
+app.register_blueprint(dishes_controller, url_prefix='/dishes')
+app.register_blueprint(users_controller, url_prefix='/users')
+
+
+@app.route('/')
+def hello():
+    return "Hello Panda"
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", 8080)
